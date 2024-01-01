448615f07a7cfc12ee502a131ec1f3f8485e23a8
coloz <coloz999@gmail.com>
2024-01-01 13:03:15 +0800

    更新

--- ios/Plugin/EsptouchPlugin.h
+++ ios/Plugin/EsptouchPlugin.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
-
+#import "ESPPacketUtils.h"
+#import "ESPProvisioner.h"
+#import "ESPProvisioningParams.h"
+#import "ESPProvisioningRequest.h"
+#import "ESPProvisioningResult.h"
+#import "ESPProvisioningUDP.h"
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
 
@@ -8,3 +13,11 @@ FOUNDATION_EXPORT const unsigned char PluginVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Plugin/PublicHeader.h>
 
+@interface esptouch2 : CDVPlugin
+@property (atomic, strong) ESPProvisioner *provisioner;
+
+- (void)start:(CDVInvokedUrlCommand*)command;
+
+- (void)stop:(CDVInvokedUrlCommand*)command;
+
+@end
--- ios/Plugin/EsptouchPlugin.m
+++ ios/Plugin/EsptouchPlugin.m
@@ -1,8 +1,61 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
+#import "EsptouchPlugin.h"
+#import "ESP_ByteUtil.h"
 
+NSString *callback_ID2;
+
+@implementation esptouch2
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(EsptouchPlugin, "Esptouch",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
 )
+
+- (void)onProvisoningScanResult:(ESPProvisioningResult *)result {
+    NSLog(@"onProvisonScanResult: address=%@, bssid=%@", result.address, result.bssid);
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        NSDictionary *device =@{@"bssid":result.bssid,@"ip":result.address};
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: device];
+        [pluginResult setKeepCallbackAsBool:true];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callback_ID2];
+        // [self.resultArray addObject:result];
+        // [self.resultView reloadData];
+        // if ([self.request.deviceCount intValue] > 0) {
+        //     // check result array size == expect size
+        //     if (self.resultArray.count >= [self.request.deviceCount intValue]) {
+        //         [self stopProvisioning:nil];
+        //     }
+        // }
+    }];
+}
+
+- (void) start:(CDVInvokedUrlCommand *)command{
+    [self.commandDelegate runInBackground:^{
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        callback_ID2 = command.callbackId;
+
+        NSString *apSsid = (NSString *)[command.arguments objectAtIndex:0];
+        NSString *apPassword = (NSString *)[command.arguments objectAtIndex:1];
+        NSString *customData = (NSString *)[command.arguments objectAtIndex:2];
+        self.provisioner = [ESPProvisioner share];
+        ESPProvisioningRequest *request = [[ESPProvisioningRequest alloc] init];
+        request.ssid = [ESP_ByteUtil getBytesByNSString:apSsid];
+        request.password = [ESP_ByteUtil getBytesByNSString:apPassword];
+        request.reservedData = [ESP_ByteUtil getBytesByNSString:customData];
+        // request.deviceCount = deviceCount;
+        // request.aesKey = aesKey;
+        [self.provisioner startProvisioning:request withDelegate:self];
+    }];
+}
+
+
+- (void) stop:(CDVInvokedUrlCommand *)command{
+    [self.provisioner stopProvisioning];
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"cancel success"];
+    [pluginResult setKeepCallbackAsBool:true];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
